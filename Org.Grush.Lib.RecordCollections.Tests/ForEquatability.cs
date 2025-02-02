9162ee8821ce5adee28f72b4a431a7f57e40ed2e
Samuel K. Grush <Samuel@Grush.org>
2025-02-01 21:51:17 -0600

    test work. Introduce Moq (yeah yeah)

--- Org.Grush.Lib.RecordCollections.Tests/ForEquatability.cs
+++ Org.Grush.Lib.RecordCollections.Tests/ForEquatability.cs
@@ -1,16 +1,27 @@
+using System.Collections;
 using FluentAssertions;
 using FluentAssertions.Execution;
+using Moq;
+using Xunit.Sdk;
 
 namespace Org.Grush.Lib.RecordCollections.Tests;
 
 public class ForEquatability
 {
   [Fact]
+  public void AgainstSpan()
+  {
+    RecordCollection<int> collectionA = [1, 2, 3];
+
+    var eq = collectionA == [1, 2, 3];
+  }
+
+  [Fact]
   public void WhenContainingValueTypes()
   {
     // Assemble
-    RecordCollection<int> collectionA = [1, 2, 3];
-    RecordCollection<int> collectionB = [1, 2, 3];
+    RecordCollection<int> collectionA = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    RecordCollection<int> collectionB = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
     // Act
     var areEqual = Equals(collectionA, collectionB);
@@ -160,6 +171,216 @@ public class ForEquatability
 
       // Assert
       areEqual.Should().BeFalse();
+    }
+  }
+
+  public class Structural
+  {
+    [Fact]
+    public void ShouldReturnFalseAgainstNull()
+    {
+      RecordCollection<int> collectionA = [1, 2, 3];
+      RecordCollection<int>? collectionB = null;
+
+      var comparerMock = MakeAlwaysTrueComparer();
+
+      var areEqual = ((IStructuralEquatable)collectionA).Equals(collectionB, comparerMock.Object);
+
+      areEqual.Should().BeFalse();
+      comparerMock.Verify(m => m.Equals(It.IsAny<int>(), It.IsAny<long>()), Times.Never);
+      comparerMock.Verify(m => m.GetHashCode(It.IsAny<object>()), Times.Never);
+    }
+
+    [Fact]
+    public void WhenComparerIsOfT_AndOtherIsRecordCollectionOfT_ShouldUseTComparer()
+    {
+      RecordCollection<byte> collectionA = [1, 2, 3];
+      RecordCollection<byte>? collectionB = [1, 2, 3];
+
+      var comparerMock = MakeComparerOf<byte>(null, null);
+      comparerMock.Setup(c => c.Equals(It.IsAny<byte>(), It.IsAny<byte>())).Returns(true);
+
+      var areEqual = ((IStructuralEquatable)collectionA).Equals(collectionB, (IEqualityComparer)comparerMock.Object);
+
+      areEqual.Should().BeTrue();
+
+      comparerMock.Verify(c => c.Equals(It.IsAny<byte>(), It.IsAny<byte>()), Times.Exactly(3));
+    }
+
+    [Fact]
+    public void WhenComparerIsOfT_AndOtherIsIEnumerableOfT_ShouldUseTComparer()
+    {
+      RecordCollection<byte> collectionA = [1, 2, 3];
+      IEnumerable<byte> enumerableB = Enumerable.Range(1, 3).Select(x => (byte)x);
+      var comparer = MakeComparerOf<byte>(null, null);
+      comparer.Setup(c => c.Equals(It.IsAny<byte>(), It.IsAny<byte>())).Returns((byte a, byte b) => a == b);
+
+      var areEqual = ((IStructuralEquatable)collectionA).Equals(enumerableB, (IEqualityComparer)comparer.Object);
+
+      areEqual.Should().BeTrue();
+
+      comparer.Verify(c => c.Equals(It.IsAny<byte>(), It.IsAny<byte>()), Times.Exactly(3));
+    }
+
+    [Fact]
+    public void WhenComparerIsOfT_ButOtherIsNotEnumerableOfT_ShouldFallbackToStructuralEquatable()
+    {
+      RecordCollection<uint> collectionA = [1, 2, 3];
+      RecordCollection<int> collectionB = [1, 2, 3];
+      var comparer = MakeComparerOf<uint>(null, null);
+      comparer.Setup(c => c.GetHashCode(It.IsAny<uint>())).Returns((uint i) => (int)i);
+      comparer.As<IEqualityComparer>().Setup(c => c.GetHashCode(It.IsIn(1, 2, 3))).Returns((int i) => i);
+
+      var areEqual = ((IStructuralEquatable)collectionA).Equals(collectionB, (IEqualityComparer)comparer.Object);
+
+      areEqual.Should().BeTrue();
+
+      comparer.Verify(c => c.GetHashCode(It.IsAny<uint>()), Times.Exactly(3));
+      comparer.As<IEqualityComparer>().Verify(c => c.GetHashCode(It.IsAny<object>()), Times.Exactly(3));
+    }
+
+    [Fact]
+    public void WhenComparerIsOfT_ButOtherIsNotEnumerableOfT_ShouldFallbackToStructuralEquatable_AndFailForMismatch()
+    {
+      RecordCollection<uint> collectionA = [1, 2, 3];
+      RecordCollection<int> collectionB = [1, 22, 3];
+      var comparer = MakeComparerOf<uint>(null, null);
+      comparer.Setup(c => c.GetHashCode(It.IsAny<uint>())).Returns((uint i) => (int)i);
+      comparer.As<IEqualityComparer>().Setup(c => c.GetHashCode(It.IsIn(1, 22, 3))).Returns((int i) => i);
+
+      var areEqual = ((IStructuralEquatable)collectionA).Equals(collectionB, (IEqualityComparer)comparer.Object);
+
+      areEqual.Should().BeFalse();
+
+      comparer.Verify(c => c.GetHashCode(It.IsAny<uint>()), Times.Exactly(3));
+      comparer.As<IEqualityComparer>().Verify(c => c.GetHashCode(It.IsAny<object>()), Times.Exactly(3));
+    }
+
+    [Fact]
+    public void WhenOtherIsEnumerableOfOther_UseComparerEquals()
+    {
+      RecordCollection<uint> collectionA = [1, 2, 3];
+      IEnumerable<double> enumerableB = Enumerable.Range(1, 3).Select(x => x + 0.4);
+      IntegralComparer comparer = new();
+
+      var areEqual = ((IStructuralEquatable)collectionA).Equals(enumerableB, comparer);
+
+      areEqual.Should().BeTrue();
+    }
+
+    [Theory]
+    [InlineData(3, 4)]
+    [InlineData(4, 3)]
+    public void WhenOtherIsEnumerableOfOther_UseComparerEquals_AndFailForMismatchedLength(int len1, int len2)
+    {
+      RecordCollection<int> collectionA = Enumerable.Range(1, len1).ToRecordCollection();
+      IEnumerable<long> enumerableB = Enumerable.Range(1, len2).Select(x => (long)x);
+      var comparerMock = MakeAlwaysTrueComparer();
+
+      var areEqual = ((IStructuralEquatable)collectionA).Equals(enumerableB, comparerMock.Object);
+
+      areEqual.Should().BeFalse();
+      comparerMock.Verify(m => m.Equals(It.IsAny<int>(), It.IsAny<long>()), Times.Exactly(3));
+      comparerMock.Verify(m => m.GetHashCode(It.IsAny<object>()), Times.Never);
+    }
+
+    [Fact]
+    public void WhenOtherIsEnumerableOfOther_UseComparerEquals_AndFailForMismatchedValues()
+    {
+      RecordCollection<int> collectionA = [5, 6, 7];
+      IEnumerable<long> enumerableB = Enumerable.Range(1, 3).Select(x => (long)x);
+      IntegralComparer comparer = new();
+
+      var areEqual = ((IStructuralEquatable)collectionA).Equals(enumerableB, comparer);
+
+      areEqual.Should().BeFalse();
+    }
+
+    [Fact]
+    public void FailForNonEnumerable()
+    {
+      RecordCollection<int> collectionA = [1, 2, 3];
+      object otherThing = new { unique = true };
+      Mock<IEqualityComparer> comparer = new(MockBehavior.Strict);
+
+      var areEqual = ((IStructuralEquatable)collectionA).Equals(otherThing, comparer.Object);
+
+      areEqual.Should().BeFalse();
+    }
+
+    [Fact]
+    public void ShortCircuitForDifferentSizedCollections()
+    {
+      RecordCollection<int> collectionA = [1, 2, 3];
+
+      var maxCollectionMoq = new Mock<IReadOnlyCollection<int>>(MockBehavior.Strict);
+      maxCollectionMoq.SetupGet(x => x.Count).Returns(int.MaxValue);
+      maxCollectionMoq.Setup(x => x.GetEnumerator()).Throws(() => FailException.ForFailure("called GetEnumerator"));
+
+      var comparerMock = MakeAlwaysTrueComparer();
+
+      var areEqual = ((IStructuralEquatable)collectionA).Equals(maxCollectionMoq.Object, comparerMock.Object);
+
+      areEqual.Should().BeFalse();
+    }
+
+
+    private Mock<IEqualityComparer> MakeComparer(IEqualityComparer? basedOn)
+    {
+      var mock = new Mock<IEqualityComparer>(MockBehavior.Strict);
+
+      if (basedOn is not null)
+      {
+        mock
+          .Setup(c => c.Equals(It.IsAny<object>(), It.IsAny<object>()))
+          .Callback((object a, object b) => basedOn.Equals(a, b));
+
+        mock
+          .Setup(c => c.GetHashCode(It.IsAny<object>()))
+          .Callback((object a) => basedOn.GetHashCode(a));
+      }
+
+      return mock;
+    }
+
+    private Mock<IEqualityComparer<T>> MakeComparerOf<T>(IEqualityComparer<T>? basedOnOfT, IEqualityComparer? basedOn)
+    {
+      var mock = MakeComparer(basedOn);
+      var comparerOfT = mock.As<IEqualityComparer<T>>();
+
+      if (basedOnOfT is not null)
+      {
+        comparerOfT
+          .Setup(c => c.Equals(It.IsAny<T>(), It.IsAny<T>()))
+          .Callback((T? a, T? b) => basedOnOfT.Equals(a, b));
+
+        comparerOfT
+          .Setup(c => c.GetHashCode(It.IsAny<T>()!))
+          .Callback((T a) => basedOnOfT.GetHashCode(a));
+      }
+
+      return comparerOfT;
+    }
+
+    private Mock<IEqualityComparer> MakeAlwaysTrueComparer()
+    {
+      var mock = new Mock<IEqualityComparer>(MockBehavior.Strict);
+      mock.Setup(x => x.Equals(It.IsAny<object>(), It.IsAny<object>())).Returns(true);
+      mock.Setup(x => x.GetHashCode(It.IsAny<object>())).Returns(1);
+      return mock;
+    }
+
+    private class IntegralComparer : IEqualityComparer
+    {
+      bool IEqualityComparer.Equals(object? x, object? y) =>
+        (x, y) switch
+        {
+          (null, null) => true,
+          (not null, not null) => Convert.ToInt64(x) == Convert.ToInt64(y),
+          _ => false,
+        };
+
+      public int GetHashCode(object obj) => Convert.ToInt64(obj).GetHashCode();
     }
   }
 }
