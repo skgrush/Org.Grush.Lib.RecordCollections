47959471ac9ce57feed6a785acbd857f722b8086
Samuel K. Grush <Samuel@Grush.org>
2025-02-04 10:18:16 -0600

    Improve serializer documentation.

--- Org.Grush.Lib.RecordCollections/Shims.cs
+++ Org.Grush.Lib.RecordCollections/Shims.cs
@@ -5,7 +5,7 @@ namespace Org.Grush.Lib.RecordCollections;
 /// <summary>
 /// Shim for
 /// <see href="https://learn.microsoft.com/en-us/dotnet/api/system.diagnostics.codeanalysis.notnullwhenattribute?view=netstandard-2.1">NotNullWhenAttribute</see>
-/// from the stdlib. Not worth bringin in the packaged version.
+/// from the stdlib. Not worth bringing in the packaged version.
 /// </summary>
 [AttributeUsage(AttributeTargets.Parameter, AllowMultiple = false, Inherited = false)]
 internal sealed class NotNullWhenAttribute(bool returnValue) : Attribute
