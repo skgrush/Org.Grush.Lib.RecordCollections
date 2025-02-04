c8045ee8cd8f05756ec1bb7dcbd407055579f196
Samuel K. Grush <Samuel@Grush.org>
2025-02-03 22:32:54 -0600

    linq extensions

--- Org.Grush.Lib.RecordCollections/RecordCollectionExtensions.cs
+++ Org.Grush.Lib.RecordCollections/RecordCollectionExtensions.cs
@@ -4,35 +4,32 @@ namespace Org.Grush.Lib.RecordCollections;
 
 public static class RecordCollectionExtensions
 {
-  public static bool SequenceEquals<TDerived, TBase>(this RecordCollection<TBase> source, RecordCollection<TDerived> other, IEqualityComparer<TBase>? comparer = null)
+  public static bool SequenceEqual<TDerived, TBase>(this RecordCollection<TBase> source, RecordCollection<TDerived> other, IEqualityComparer<TBase>? comparer = null)
     where TDerived : TBase
     => ((ImmutableArray<TBase>)source).SequenceEqual((ImmutableArray<TDerived>)other, comparer);
 
-  public static bool SequenceEquals<TDerived, TBase>(this RecordCollection<TBase> source, IEnumerable<TDerived> other, IEqualityComparer<TBase>? comparer = null)
+  public static bool SequenceEqual<TDerived, TBase>(this RecordCollection<TBase> source, IEnumerable<TDerived> other, IEqualityComparer<TBase>? comparer = null)
     where TDerived : TBase
     => ((ImmutableArray<TBase>)source).SequenceEqual(other, comparer);
 
-  public static bool SequenceEquals<TDerived, TBase>(this RecordCollection<TBase> source, RecordCollection<TDerived> other, Func<TBase, TBase, bool> predicate)
+  public static bool SequenceEqual<TDerived, TBase>(this RecordCollection<TBase> source, RecordCollection<TDerived> other, Func<TBase, TBase, bool> predicate)
     where TDerived : TBase
     => ((ImmutableArray<TBase>)source).SequenceEqual((ImmutableArray<TDerived>)other, predicate);
-
-  public static RecordCollection<
-#nullable disable
-    TResult
-#nullable restore
-  > Cast<TBase, TResult>(this RecordCollection<TBase> source)
-    where TResult : class?
-  {
-    var src = (ImmutableArray<TBase>)source;
-
-    return (RecordCollection<TResult>)src.As<TResult>();
-  }
 
   public static IEnumerable<TResult> Select<T, TResult>(this RecordCollection<T> source, Func<T, TResult> selector)
     => ((ImmutableArray<T>)source).Select(selector);
 
   public static IEnumerable<T> Where<T>(this RecordCollection<T> source, Func<T, bool> predicate)
     => ((ImmutableArray<T>)source).Where(predicate);
+
+  public static Dictionary<TKey, T> ToDictionary<TKey, T>(
+    this RecordCollection<T> source,
+    Func<T, TKey> keySelector,
+    IEqualityComparer<TKey>? comparer = null
+  ) where TKey : notnull
+  {
+    return ((ImmutableArray<T>)source).ToDictionary(keySelector, t => t, comparer);
+  }
 
   public static Dictionary<TKey, TElement> ToDictionary<TKey, TElement, T>(
     this RecordCollection<T> source,
